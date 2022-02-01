35d2426db9de3999a8d467bc0677054992d326a5
sz3lbi <8168846+sz3lbi@users.noreply.github.com>
2022-02-01 20:46:06 +0100

    add `max_overflow` property for SQLAlchemy

--- app/db.py
+++ app/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import registry, sessionmaker
 
 from app.core.config import settings
 
-engine = create_engine(settings.database_url, future=True, pool_size=10)
+engine = create_engine(settings.database_url, future=True, pool_size=15, max_overflow=5)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 mapper_registry = registry()

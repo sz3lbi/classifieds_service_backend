692147bcaa463addc10d9ed0eefd3bab2663f58f
sz3lbi <8168846+sz3lbi@users.noreply.github.com>
2022-02-01 20:42:02 +0100

    add `pool_size` for SQLAlchemy

--- app/db.py
+++ app/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import registry, sessionmaker
 
 from app.core.config import settings
 
-engine = create_engine(settings.database_url, future=True)
+engine = create_engine(settings.database_url, future=True, pool_size=10)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 mapper_registry = registry()
